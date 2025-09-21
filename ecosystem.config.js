18b38c1e0c1be5cf108db3bf18aa4b896c6ed0b9
Aniekan Offiong <ekanoffiong@gmail.com>
2025-09-22 01:42:37 +0200

    Debugging memory usage

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,11 @@ module.exports = {
       exp_backoff_restart_delay: 100,
       max_restarts: 10,
       min_uptime: 2000,
+      // Monitor memory usage
+      pmx: true,
+      // Log memory usage
+      log_type: 'json',
+      merge_logs: true,
       node_args: '--max-old-space-size=2048', // Set Node.js heap size
       env: {
         NODE_ENV: 'production'
