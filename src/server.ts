b0d26980a38916f0c30dad95a17058739c8cf4c1
Aniekan Offiong <ekanoffiong@gmail.com>
2025-09-22 02:16:35 +0200

    Fixed issue with pem files

--- src/server.ts
+++ src/server.ts
@@ -9,16 +9,16 @@ dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-const options = {
-  key: fs.readFileSync('localhost-key.pem'),
-  cert: fs.readFileSync('localhost.pem')
-};
-
 AppDataSource.initialize()
   .then(() => {
     console.log("Data Source has been initialized for environment: ", process.env.NODE_ENV);
 
     if (process.env.NODE_ENV !== 'production') {
+      const options = {
+        key: fs.readFileSync('localhost-key.pem'),
+        cert: fs.readFileSync('localhost.pem')
+      };
+
       https.createServer(options, app).listen(PORT, () => {
         console.log(`Server running on https://localhost:${PORT}`);
       });
