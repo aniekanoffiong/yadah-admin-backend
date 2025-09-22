a360eb0fcf112c2ae568ba19c5af1df48a97fe96
Aniekan Offiong <ekanoffiong@gmail.com>
2025-09-22 02:22:22 +0200

    uncomment redis setting for production password use

--- src/external/redis.client.ts
+++ src/external/redis.client.ts
@@ -3,7 +3,7 @@ import Redis from 'ioredis';
 const redis = new Redis({
   host: process.env.REDIS_HOST || 'localhost',
   port: Number(process.env.REDIS_PORT) || 6379,
-  // password: process.env.REDIS_PASSWORD, // if you have auth
+  password: process.env.REDIS_PASSWORD, // if you have auth
 });
 
 export default redis;
